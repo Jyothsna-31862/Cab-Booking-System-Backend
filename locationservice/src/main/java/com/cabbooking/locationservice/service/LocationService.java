83a1bde3adfe1d57fe70c82c1af59a750397662b
Suresh Pilli <Suresh.Pilli@cognizant.com>
2025-09-11 09:00:39 +0530

    Feat: Added get Location API

--- locationservice/src/main/java/com/cabbooking/locationservice/service/LocationService.java
+++ locationservice/src/main/java/com/cabbooking/locationservice/service/LocationService.java
@@ -1,9 +1,9 @@
 package com.cabbooking.locationservice.service;
 
-import com.cabbooking.locationservice.model.Location;
+import com.cabbooking.locationservice.dto.LocationDto;
 
 import java.util.List;
 
 public interface LocationService {
-    List<Location> getLocations();
+    List<LocationDto> getLocations();
 }
--- locationservice/src/main/java/com/cabbooking/locationservice/service/LocationServiceImp.java
+++ locationservice/src/main/java/com/cabbooking/locationservice/service/LocationServiceImp.java
@@ -1,22 +1,31 @@
 package com.cabbooking.locationservice.service;
 
-import com.cabbooking.locationservice.model.Location;
+import com.cabbooking.locationservice.dto.LocationDto;
 import com.cabbooking.locationservice.repository.LocationRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LocationServiceImp implements LocationService{
 
     private final LocationRepository locationRepository;
+    private final ModelMapper modelMapper;
 
-    public LocationServiceImp(LocationRepository locationRepository) {
+    public LocationServiceImp(LocationRepository locationRepository, ModelMapper modelMapper) {
         this.locationRepository = locationRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public List<Location> getLocations() {
-        return locationRepository.findAll();
+    public List<LocationDto> getLocations() {
+        return locationRepository.findAll()
+                .stream()
+
+                .map(location -> modelMapper.map(location, LocationDto.class))
+                .collect(Collectors.toList());
     }
+
 }
