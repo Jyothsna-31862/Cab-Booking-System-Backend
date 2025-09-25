82bec7f4351dc2ecbeee2aec68cf6869f095c35e
YenumulaJyothsna <yenumula.jyothsnapriyadurga@cognizant.com>
2025-09-25 17:15:46 +0530

    Fix: Changed Dependency in api gateway

--- apigateway/src/main/java/com/cabbooking/apigateway/client/AuthServiceClient.java
+++ apigateway/src/main/java/com/cabbooking/apigateway/client/AuthServiceClient.java
@@ -1,5 +1,6 @@
 package com.cabbooking.apigateway.client;
 
+import com.cabbooking.apigateway.controller.FallbackController;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
